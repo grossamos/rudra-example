d9c3ed31a522874c419eb163efb8a6d8571c399d
grossamos <email@amosgross.com>
2022-07-17 11:00:59 +0200

    Added workflow step for rudra

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"rudra-example/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
-
 // @title           Rudra Example Project
 // @version         1.0
 // @description     This is a sample project for the rudra test tool
 
-// @license.name  BSD-2-Clause 
+// @license.name  BSD-2-Clause
 // @license.url   https://raw.githubusercontent.com/grossamos/rudra/main/LICENSE
 
 // @host      localhost:8080
 // @BasePath  /
 func main() {
+    gin.SetMode(gin.ReleaseMode)
+
     r := gin.Default()
 
     r.GET("/", controller.BaseHandler)
     r.GET("/weather", controller.GetCurrentWeather)
     r.POST("/validate", controller.ValidateWeather)
 
+    fmt.Println("Running on port 8080")
     r.Run(":8080")
 }
